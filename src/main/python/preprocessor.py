dd8783000c490bb92a5fb9ae295db20343a55dbb
haeun <hekim@puzzledata.com>
2023-06-28 22:40:24 +0900

    정합성 이슈가 있는 id의 상세 정보 확인 함수 추가

--- src/main/python/preprocessor.py
+++ src/main/python/preprocessor.py
@@ -7,15 +7,33 @@ class Preprocessor:
         self.params = params
 
     def check_data_consistency(self):
-        info = self.params["preprocess"]["consistency"]
-        abnormal_cases = dict()
-        for key in info:
-            df_groupby = self.data.groupby(key)[info[key]].nunique()
-            df_groupby["sum"] = df_groupby.sum(axis=1)
-            abnormal_cases[key] = df_groupby.loc[
-                df_groupby["sum"] > len(info[key])
-            ].index.tolist()
-        return abnormal_cases
+        def generate_abnormal_cases():
+            info = self.params["preprocess"]["consistency"]
+            abnormal_cases = dict()
+            for key in info:
+                df_groupby = self.data.groupby(key)[info[key]].nunique()
+                df_groupby["sum"] = df_groupby.sum(axis=1)
+                abnormal_cases[key] = df_groupby.loc[
+                    df_groupby["sum"] > len(info[key])
+                ].index.tolist()
+            return abnormal_cases
+
+        def get_case_details(case_list):
+            details = dict()
+            for key in case_list:
+                if case_list[key]:
+                    related_keys = params["preprocess"]["consistency"][key]
+                    n_data = self.data.loc[data[key].isin(case_list[key])]
+                    details[key] = (
+                        n_data.groupby(key)
+                        .agg({rkey: "unique" for rkey in related_keys})
+                        .to_dict("index")
+                    )
+            return details
+
+        abnormal_cases = generate_abnormal_cases()
+        abnormal_details = get_case_details(abnormal_cases)
+        return abnormal_details
 
 
 if __name__ == "__main__":
